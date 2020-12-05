7071e899df105fae3003ac151f29e09ccdc6363b
MuhamadAzizi <muhamadazizi177@yahoo.com>
2020-12-05 21:51:29 +0700

    Update on menu's component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,25 +1,32 @@
 import React from 'react'
+import Draggable from 'react-draggable'
 import { NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 function Menu() {
     return(
-        <div style={{position: 'fixed', bottom: '10px', left: '10px'}}>
-            <div class="btn-group dropup">
-                <button style={{opacity: '0.5'}} type="button" class="btn btn-dark rounded" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <FontAwesomeIcon icon={faBars} />
-                </button>
-                <div class="dropdown-menu text-center">
-                    <NavLink to="/" style={{borderLeft: 'none', borderRight: 'none', borderRadius: '0'}} className="nav btn card col-12 mb-2 font-paragraph text-dark">Home</NavLink>
-                    <NavLink to="/profil" style={{borderLeft: 'none', borderRight: 'none', borderRadius: '0'}} className="nav btn card col-12 mb-2 font-paragraph text-dark">Profil</NavLink>
-                    <NavLink to="/berita" style={{borderLeft: 'none', borderRight: 'none', borderRadius: '0'}} className="nav btn card col-12 mb-2 font-paragraph text-dark">Berita</NavLink>
-                    <NavLink to="/jadwal-sholat" style={{borderLeft: 'none', borderRight: 'none', borderRadius: '0'}} className="nav btn card col-12 mb-2 font-paragraph text-dark">Jadwal Sholat</NavLink>
-                    <a href="https://www.loker.id/lokasi-pekerjaan/banten" style={{borderLeft: 'none', borderRight: 'none', borderRadius: '0'}} className="nav btn card col-12 mb-2 font-paragraph text-dark" target="_blank" rel="noopener noreferrer">Loker</a>
-                    <a href="https://www.bantenprov.go.id/" style={{borderLeft: 'none', borderRight: 'none', borderRadius: '0'}} className="nav btn card col-12 mb-2 font-paragraph text-dark" target="_blank" rel="noopener noreferrer">bantenprov.go.id</a> 
+        <Draggable 
+            axis="y" 
+            grid={[25, 25]}
+            scale={1}>
+            <div style={{position: 'fixed', bottom: '10px', right: '10px'}}>
+                <div class="btn-group dropup">
+                    <button style={{width: '52px', height: '52px'}} type="button" className="btn btn-light border border-dark rounded-circle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        <FontAwesomeIcon icon={faBars} size="lg" />
+                    </button>
+                    <div class="dropdown-menu border border-dark">
+                        {/* <NavLink to="/" className="dropdown-item text-dark ">Home</NavLink> */}
+                        <NavLink to="/profil" className="dropdown-item">Profil</NavLink>
+                        <NavLink to="/berita" className="dropdown-item">Berita</NavLink>
+                        <NavLink to="/cuaca" className="dropdown-item">Cuaca</NavLink>
+                        <NavLink to="/jadwal-sholat" className="dropdown-item">Jadwal Sholat</NavLink>
+                        <a href="https://www.loker.id/lokasi-pekerjaan/banten" className="dropdown-item" target="_blank" rel="noopener noreferrer">Loker</a>
+                        <a href="https://www.bantenprov.go.id/" className="dropdown-item" target="_blank" rel="noopener noreferrer">bantenprov.go.id</a> 
+                    </div>
                 </div>
             </div>
-        </div>
+        </Draggable>
     )
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,8 +7,8 @@ function Navigation() {
             <div className="col-12 col-lg-11 bg-primary-btnvrs rounded text-light py-2" style={{width: '100%'}}>
                 <h1 className="font-primary my-3 text-center">Navigation</h1>
                 <div>
-                    <NavLink to="/" className="nav btn btn-light col-12 mb-2 card font-paragraph text-dark">Home</NavLink>
-                    <NavLink to="/profil" className="nav btn btn-light col-12 mb-2 card font-paragraph text-dark">Profil</NavLink>
+                    {/* <NavLink to="/" className="nav btn btn-light col-12 mb-2 card font-paragraph text-dark">Home</NavLink> */}
+                    {/* <NavLink to="/profil" className="nav btn btn-light col-12 mb-2 card font-paragraph text-dark">Profil</NavLink> */}
                     <NavLink to="/berita" className="nav btn btn-light col-12 mb-2 card font-paragraph text-dark">Berita</NavLink>
                     <NavLink to="/jadwal-sholat" className="nav btn btn-light col-12 mb-2 card font-paragraph text-dark">Jadwal Sholat</NavLink>
                     <a href="https://www.loker.id/lokasi-pekerjaan/banten" className="nav btn btn-light col-12 mb-2 card font-paragraph text-dark" target="_blank" rel="noopener noreferrer">Loker</a>
